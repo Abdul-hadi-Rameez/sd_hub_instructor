e4b63d1ad728e84d4abc04d74bc728b23649cff3
Abdul-hadi-rameez <abdulhadirameez@gmail.com>
2024-10-24 13:03:35 +0530

    24

--- SD-Hub-Portal/src/app/registration/registration.component.ts
+++ SD-Hub-Portal/src/app/registration/registration.component.ts
@@ -17,10 +17,27 @@ export class RegistrationComponent implements OnInit {
 
 ngOnInit():void{
   this.firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
+    studentID: ['', Validators.required],
+    appDate: ['', Validators.required],
+    firstName: ['', Validators.required],
+    middleName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    f_firstname: ['', Validators.required],
+    f_middlename: ['', Validators.required],
+    f_lastname: ['', Validators.required],
+    dob: ['', Validators.required],
+    phnp: ['', Validators.required],
+    email: ['', Validators.required],
+    address: ['', Validators.required],
+    p_contact: ['', Validators.required],
+    income: ['', Validators.required],
   });
   this.secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
+    secCtrl: ['', Validators.required],
+    Degree: ['', Validators.required],
+    Collegename: ['', Validators.required],
+    Year: ['', Validators.required],
+    Percentage: ['', Validators.required],
   });
 }  
 }
